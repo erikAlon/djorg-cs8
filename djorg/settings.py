f1bfd1f5e9bdb86f96de8ae59554f96df3062dbd
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-06-26 18:01:33 -0500

    added admin

--- djorg/settings.py
+++ djorg/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes',
+    'bookmark',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
